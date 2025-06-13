b1fd8aa520f1257a4321d90d91d7ae8491127be4
Krushna <ankadekrushna01@gmail.com>
2025-06-13 13:43:07 +0530

    learn input types and learn semantic html

---
+++ JavaScript/loops.js
@@ -0,0 +1,14 @@
+let arr = [12, 4, 5, 66, 50, 9, 44, 40];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     console.log(arr[i]);
+//   }
+// }
+let i = 0
+while(i<arr.length){
+    if(arr[i]%2 ==0){
+        console.log(arr[i]);
+        
+    }
+    i++
+}
