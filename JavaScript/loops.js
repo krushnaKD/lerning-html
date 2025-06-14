0f1f0f4b90a51cb86b68d26400cfa1a8b65f8f48
Krushna <ankadekrushna01@gmail.com>
2025-06-14 22:07:24 +0530

    practice loops in js

--- JavaScript/loops.js
+++ JavaScript/loops.js
@@ -1,14 +1,121 @@
-let arr = [12, 4, 5, 66, 50, 9, 44, 40];
+// let arr = [12, 4, 5, 66, 50, 9, 44, 40];
 // for (let i = 0; i < arr.length; i++) {
 //   if (arr[i] % 2 === 0) {
 //     console.log(arr[i]);
 //   }
 // }
-let i = 0
-while(i<arr.length){
-    if(arr[i]%2 ==0){
-        console.log(arr[i]);
+//2
+
+// let i = 0
+// while(i<arr.length){
+//     if(arr[i]%2 ==0){
+//         console.log(arr[i]);
+
+//     }
+//     i++
+// }
+3;
+//
+// let arr = [12, 33, 45, 8, 97, 0, 76];
+
+// let searchelement = (no) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == no) {
+//       console.log(i);
+//     }
+//   }
+//   return -1
+// };
+
+// let res = searchelement(6);
+// console.log(res);
+
+//
+//4
+
+// let arr = [12, 33, 45, 8, -7, 0, -76];
+// let count=0;
+// function getnegativeValue(){
+//   for(let i = 0;i<arr.length;i++){
+//     if(arr[i]<0){
+//         count = count + 1
+    
+//     }
+//   }
+//   return 1
+// }
+// getnegativeValue()
+// console.log(count);
+
+//5
+
+// let arr = [12, 33, 45, 8, -7, 0, 76];
+
+// function largestNumber(){
+//     let large=0;
+//     for(let i = 0;i<arr.length;i++){
+//      if(arr[i]>large){
+//         large=arr[i]
+//      }
+//     }
+//     console.log(large);
+    
+// }
+// largestNumber()
+
+// for(let i=0;i<3;i++){
+//     for(let j=0;j<i;j++){
+//         console.log("i="  ,i , "j =" ,j);
         
+//     }
+// }
+
+// for(let i = 0;i<5;i++){
+//     for(let j=0;j<=i;j++){
+//         console.log("i="  ,i , "j =" ,j);
+//     }
+// }
+
+// for(let i= 0;i<3;i++){
+//     for(let j = i;j>0;j--){
+//         console.log("i="  ,i , "j =" ,j);
+//     }
+// }
+
+// for(let i = 0;i<3;i++){
+//     for(let j=i;j>=0;j--){
+//         console.log("i="  ,i , "j =" ,j);
+        
+//     }
+// }
+
+// for(let i=5;i>0;i--){
+//     for(let j = 0;j<i;j++){
+//          console.log("i="  ,i , "j =" ,j);
+//     }
+// }
+//Q
+// ****
+// ****
+// ****
+// ****
+
+// for(let i = 0;i<4;i++){
+//     let row = ""
+//     for(let j = 0;j<4;j++){
+//         row = row + "*";
+        
+//     }
+//     console.log(row);
+// }
+
+//Q
+
+for(let i = 0;i<4;i++){
+    let rightangle = " "
+    for(let j = 0;j<i+1;j++){
+        rightangle = rightangle + "*"
     }
-    i++
+    console.log(rightangle);
+    
 }
